130d99aff3cc53532c1335c302229a90189e2e9c
Mayck Diego Cipriano <mayck.diego@ea.org.br>
2021-11-03 18:20:08 -0300

    Gravação de novos cursos

--- src/components/courses/form_course.js
+++ src/components/courses/form_course.js
@@ -1,27 +1,64 @@
-import { Button, Container, Icon, Switch, Textarea, TextInput } from "react-materialize"
+import axios from "axios";
+import { useState } from "react";
+import { Button, Icon, Switch, Textarea, TextInput } from "react-materialize"
+import { useHistory } from "react-router"
 
 
-const FormCourse = () =>(
-    <Container>
+const FormCourse = () =>{
+    const history = useHistory();
+    const [dadosColecao, setDadosColecao] = useState({
+        colecaoId: 0,
+        nome: '',
+        descricao: '',
+        publico: false
+    });
+
+    const handlerChange = ( e ) => {
+        const valor = e.target.id === "publico" ? e.target.checked : e.target.value;
+        setDadosColecao({ ... dadosColecao, [e.target.id]: valor  });
+    };
+
+    const goBack = () =>{
+        history.push("/courses");
+    }
+
+    const handlerSubmit = (e) => {
+        console.log(dadosColecao);
+        axios.post("https://flashcard-api-mayck.herokuapp.com/api/colecoes", dadosColecao)
+        .catch( error => console.log(error) )
+        .finally(goBack)
+        ;
+
+        e.preventDefault();
+    }
+
+    return (
+    <form onSubmit={handlerSubmit}>
         <h1>Cursos</h1>
         <TextInput
             id="nome"
+            value={dadosColecao.nome}
+            onChange={ function(e){ handlerChange(e) } }
             placeholder="Nome do Curso"
             />
         <Textarea
             id="descricao"
+            value={dadosColecao.descricao}
+            onChange={ function(e){ handlerChange(e) } }
             placeholder="Descrição do Curso"
             />
         <Switch
             id="publico"
             offLabel="Privado"
-            onChange={function noRefCheck(){}}
+            checked={dadosColecao.publico}
+            onChange={ function(e){ handlerChange(e) } }
             onLabel="Publico"
             />
             <br />
         <Button
             node="button"
             waves="light"
+            onClick={goBack}
             >
             Cancelar
         </Button>
@@ -36,7 +73,8 @@ const FormCourse = () =>(
                 send
             </Icon>
         </Button>
-    </Container>
-)
+    </form>
+    )
+}
 
 export default FormCourse
